2b51ee074acbff8587fd19c88d8bf31208b611a0
kalethabh <kalethabh@gmail.com>
2023-03-14 22:15:49 -0500

    Actualizated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import './About.css'
 
 function About() {
+  const navigate = useNavigate()
   return (
     <div className='text'>
+        <button className='backToHome'onClick={() => navigate(-1)}>Volver</button>
         <h1>Bienvenidos a mi primer Single Page Application!</h1>
-        <h2>
-          Esta aplicacion es con el fin de poner en practica mis conocimientos adquiridos en el botcamp de SoyHenry.
-        </h2>
-        <h3>Mi nombre es Kaleth</h3>
+        <h2>Esta aplicacion es con el fin de poner en practica mis conocimientos adquiridos en el bootcamp de SoyHenry.</h2>
+        <h1>Mi nombre es Kaleth Benjumea</h1>
       </div>
   )
 }
